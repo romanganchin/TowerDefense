1fef60a964a825854223a745d2cd2ff4a8ef620b
sorloff <sorloff@umass.edu>
2016-12-04 19:54:44 -0500

    added functionality for moving creepers

--- tower_defense/scripts/creeper.py
+++ tower_defense/scripts/creeper.py
@@ -17,16 +17,42 @@ creepers       = [] #each creeper should be [health, index of location in path]
 path           = []
 
 #returns a list of points, which are the steps the creepers take at each timestep
-#the list should only be used by this node, but might be otherwise useful
+#the path coords should only be used by this node, but might be useful for something
+#TODO: test this to make sure it actually works
 def MakePathService(req):
-	p = req.point_cloud
-	pass
+	p     = req.point_cloud
+	start = req.start
+	end   = req.end
 
+#every time this is called the creepers will all move forward one step
 #create_new = True -> put a new creeper at the start point
 #so call this 20 times to make 20 creepers
 #you'll probably want to stagger the calls to this so they don't overlap
+#TODO: test this to make sure it actually works
 def MoveCreepersService(req):
-	create_new = req.create_new
+	create_new        = req.create_new
+	current_locations = []
+	reached_end       = False
+
+	if create_new:
+		new_creeper = [[creeper_health, -1]]
+		creepers    = new_creeper + creepers
+
+	for creeper in creepers:
+		if not creeper[1] == len(path) - 1:
+			creeper[1] += 1
+			current_locations.append(path[creeper[1]])
+		else:
+			reached_end = True
+
+	#if a creeper reached the end, delete it from the list of creepers
+	#because of the way creepers are created, the last/closest to the end creeper
+	#is always at the end of the list
+	if reached_end:
+		creepers = creepers[:-1]
+
+	return MoveCreepersSrvResponse(current_locations, reached_end)
+
 
 
 if __name__ == "__main__":
